5d30b1fac1011630626506f9c4775e18c420895b
Aniket Kumar Pandey <meet.aniket.kumar@gmail.com>
2022-04-27 19:00:08 +0530

    Improved: code to add a css class show that we do expand whole card based on each other (#28zuw3w).

--- src/views/SelectFacility.vue
+++ src/views/SelectFacility.vue
@@ -219,6 +219,10 @@ export default defineComponent({
   border-right: 1px solid var(--ion-color-medium);
 }
 
+.section-grid {
+  align-items: start;
+}
+
 ion-item-divider > ion-checkbox {
    margin-inline-end: 16px;
 }
