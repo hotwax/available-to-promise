923f4103c2442effc95882994e239685ce90d0d7
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-05-04 18:50:20 +0530

    Implemented: code to define the service to get the shopify config(#29bdb9f)

--- src/services/UtilService.ts
+++ src/services/UtilService.ts
@@ -9,6 +9,15 @@ const getServiceStatusDesc = async (payload: any): Promise<any> => {
   });
 }
 
+const getShopifyConfig = async (payload: any): Promise <any>  => {
+  return api({
+    url: "performFind",
+    method: "post",
+    data: payload
+  });
+}
+
 export const UtilService = {
-  getServiceStatusDesc
+  getServiceStatusDesc,
+  getShopifyConfig
 }
