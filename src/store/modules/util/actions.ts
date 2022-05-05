9d385ac8fb9ebee4c89caf6fa50aae0c7bebbfe4
Aniket Kumar Pandey <meet.aniket.kumar@gmail.com>
2022-05-05 11:19:07 +0530

    Improved: code to revert unwanted changes from 'UtilService' and 'util/actions.ts' files (#28zuw3w).

--- src/store/modules/util/actions.ts
+++ src/store/modules/util/actions.ts
@@ -1,17 +1,17 @@
+import { UtilService } from '@/services/UtilService'
 import { ActionTree } from 'vuex'
 import RootState from '@/store/RootState'
-import * as types from './mutation-types'
-import { hasError, showToast } from '@/utils'
 import UtilState from './UtilState'
-import { UtilService } from '@/services/UtilService'
+import * as types from './mutation-types'
+import { hasError } from '@/utils'
 import { translate } from '@/i18n'
 
 const actions: ActionTree<UtilState, RootState> = {
   /**
-     * Status Description
-     */
-  async getServiceStatusDesc({ commit }) {
-    try {
+   * Status Description
+   */
+  async getServiceStatusDesc ({ commit }) {
+    try{
       const resp = await UtilService.getServiceStatusDesc({
         "inputFields": {
           "statusTypeId": "SERVICE_STATUS",
@@ -21,11 +21,11 @@ const actions: ActionTree<UtilState, RootState> = {
         "fieldList": ["statusId", "description"],
         "noConditionFind": "Y",
         "viewSize": 20
-      })
+      }) 
       if (resp.status === 200 && !hasError(resp) && resp.data.count) {
         commit(types.UTIL_SERVICE_STATUS_DESC_UPDATED, resp.data.docs);
       }
-    } catch (err) {
+    } catch(err) {
       console.error(err)
     }
   },
