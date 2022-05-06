1f14b234e7d1f878b5ad7ece7bb819f756ea5aeb
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-05-06 18:08:25 +0530

    Implemented: separate mutation for reset filters and improved name when updating the filters(#29wm19n)

--- src/store/modules/product/mutations.ts
+++ src/store/modules/product/mutations.ts
@@ -8,11 +8,14 @@ const mutations: MutationTree <ProductState> = {
     state.products.total.variant = payload.totalVariant;
     state.products.total.virtual = payload.totalVirtual;
   },
-  [types.PRODUCT_APPLIED_FILTERS_UPDATED] (state, payload) {
+  [types.PRODUCT_FILTER_UPDATED] (state, payload) {
     (state.appliedFilters as any)[payload.type][payload.id] = payload.value
   },
   [types.PRODUCT_QUERY_UPDATED] (state, payload) {
     state.query = payload
+  },
+  [types.PRODUCT_FILTERS_UPDATED] (state, payload) {
+    (state.appliedFilters as any)[payload.type] = payload.value
   }
 }
 export default mutations;
