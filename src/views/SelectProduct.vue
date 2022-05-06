5c3db792a2875eee0a21eb63935836fec7f6a656
niharika <niharika2nas@gmail.com>
2022-05-06 15:39:32 +0530

    Reset button and List header label should have space between them(#2a22y4d)

--- src/views/SelectProduct.vue
+++ src/views/SelectProduct.vue
@@ -25,8 +25,10 @@
               <ion-input type="number" :placeholder="$t('global threshold')" v-model="threshold"/>
             </ion-item>
             <ion-list-header>
+              <div>
               <h3>{{ $t("Include") }}</h3>
               <ion-button fill="clear" color="warning">{{ $t('reset') }}</ion-button>
+              </div>
             </ion-list-header>
             <ion-card>
               <ion-toolbar>
@@ -67,8 +69,10 @@
           </ion-list>
           <ion-list>
             <ion-list-header>
+              <div>
               <h3>{{ $t("Exclude") }}</h3>
               <ion-button fill="clear" color="warning">{{ $t('reset') }}</ion-button>
+              </div>
             </ion-list-header>
             <ion-card>
               <ion-toolbar>
@@ -386,15 +390,22 @@ export default defineComponent({
 
 <style scoped>
 .section-grid {
-  grid-template-columns: repeat(auto-fill, 200px);
+grid-template-columns: repeat(auto-fill, 200px);
+}
+
+ion-list-header > div{
+flex: 1;
+display: flex;
+justify-content: space-between;
 }
 
 @media (min-width: 991px) {
-  .action {
-    position: absolute;
-    bottom: 10%;
-    left: 50%;
-    transform: translate(-50%, 0);
-  }
+.action {
+position: fixed;
+z-index: 3;
+bottom: 10%;
+left: 50%;
+transform: translate(-50%, 0);
+}
 }
 </style>
