d3edb78aaf09ab67cd1b3ba7923fd7441dec0e45
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-05-09 18:39:32 +0530

    Improved: solr query to enable search on parent product name, pass q.op when saving the query and remove the action call when changing the product store(fix-solr-query)

--- src/components/SaveThresholdModal.vue
+++ src/components/SaveThresholdModal.vue
@@ -109,8 +109,10 @@ export default defineComponent({
     async saveThresholdRule() {
       const solrQuery = this.query
 
-      // removed params object from query as there is no need for grouping or pagination when storing the query
-      delete solrQuery.json.params
+      // re-initialized params object from query as there is no need for grouping or pagination when storing the query
+      solrQuery.json.params = {
+        "q.op": "AND"
+      }
       // made the query to default (*:*) before storing, as the threshold will be set for all the products those
       // are fullfilling the filters condition
       solrQuery.json['query'] = "*:*"
