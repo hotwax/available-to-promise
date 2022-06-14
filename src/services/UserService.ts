3863e9da4506e346825738be3387dbc4fe79fce0
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-06-14 11:30:25 +0530

    Improved: comment to TODO note

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -51,7 +51,8 @@ const setUserPreference = async (payload: any): Promise<any> => {
 const getUserPreference = async (payload: any): Promise<any> => {
   return api({
     url: "service/getUserPreference",
-    method: "post",//Due to security reasons service model OMS 1.0 does not support sending parameters in get request that's why we use post here
+    //TODO Due to security reasons service model OMS 1.0 does not support sending parameters in get request that's why we use post here
+    method: "post",
     data: payload,
   });
 }
