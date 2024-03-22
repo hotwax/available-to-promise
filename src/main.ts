feed07066f3c9167626123e595466cb41e87fb40
R-Sourabh <rsourabh002@gmail.com>
2024-03-22 17:08:26 +0530

    Added: usage of setLocale api

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ import permissionRules from '@/authorization/Rules';
 import permissionActions from '@/authorization/Actions';
 import { dxpComponents } from '@hotwax/dxp-components'
 import { login, logout, loader } from './user-utils';
-import { getConfig, initialise } from '@/adapter'
+import { getConfig, initialise, setUserLocale } from '@/adapter'
 import localeMessages from './locales';
 
 
@@ -60,7 +60,8 @@ const app = createApp(App)
     appLoginUrl: process.env.VUE_APP_LOGIN_URL as string,
     getConfig,
     initialise,
-    localeMessages
+    localeMessages,
+    setUserLocale
   });
 
 // Filters are removed in Vue 3 and global filter introduced https://v3.vuejs.org/guide/migration/filters.html#global-filters
