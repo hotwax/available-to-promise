2649da08e91f21879b6918fdb11ea084aa707c17
Yash Maheshwari <yashmaheshwari551@gmail.com>
2026-05-25 11:12:33 +0530

    Added: PWA support via vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import { versionInfoUtil } from '../../common/utils/versionInfoUtil'
 import pkg from './package.json'
+import { VitePWA } from 'vite-plugin-pwa'
+import manifest from "./manifest.json"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [
       vue(),
-      legacy()
+      legacy(),
+      VitePWA({
+        registerType: "autoUpdate", // Automatically updates the service worker, check if this correct to aut update or we should go with prompt support
+        selfDestroying: true, // Unregisters any existing service worker and clears cache,
+        manifest: manifest as any,
+        devOptions: {
+          enabled: true
+        }
+      })
     ],
     define: {
       'import.meta.env.VITE_APP_VERSION_INFO': JSON.stringify(JSON.stringify(versionInfoUtil.getVersionInfo(pkg.version)))
